a97bedb634ce871d73afa88fcb945e7e69086bf3
bariajb <joniever.baria@lexisnexisrisk.com>
2023-06-14 08:30:19 +0800

    Gym Logger first commit

---
+++ backend/routes/workouts.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
+const requireAuth = require('../middleware/requireAuth')
+
+
+const router = express.Router()
+
+//require auth for all workout
+router.use(requireAuth)
+
+//Create all the routes here
+
+//GET all workouts
+router.get('/', getWorkouts)
+
+//GET a single workout
+router.get('/:id', getWorkout)
+
+//POST a new workout
+router.post('/', createWorkout)
+
+//DELETE a workout
+
+router.delete('/:id', deleteWorkout)
+
+
+//UPDATE a workout
+router.patch('/:id', updateWorkout)
+
+
+//export the router
+module.exports = router
